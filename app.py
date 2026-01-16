8a55b2997c736d467eb08a23ff93ebf820e097b3
NextMoveEnergy <matjaz.grillo@nextmoveenergy.com>
2026-01-16 13:47:34 +0100

    add data retrival

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 pg = st.navigation([
     st.Page('_pages/generate_upn_xml.py', title="Generate UPN XML"),
     st.Page('_pages/retreive_meter_readings.py', title="Meter readings"),
-    st.Page('_pages/retreive_meter_readings_small_batch.py', title="Meter readings SMALL BATCH"),
+    st.Page('_pages/ceeps_mm_mt_data_retrieval.py', title="CEEPS MT&MM data retrieval"),
     st.Page('_pages/priloga_a.py', title="Priloga A 2.6"),
     st.Page('_pages/priloga_b.py', title="Priloga A 2.7"),
     #st.Page('_pages/priloga_c.py', title="Priloga A 2.7.5
